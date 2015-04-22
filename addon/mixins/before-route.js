56090d31074e4b5c2536bcf82df2c09df5c88b31
Kelly Selden <kellyselden@gmail.com>
2015-04-21 23:07:12 -0400

    insert locale when missing

--- addon/mixins/before-route.js
+++ addon/mixins/before-route.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Mixin.create({
   beforeModel: function() {
-    this.transitionToLocale('home');
+    this.transitionToLocale(this.get('localeRoute') || 'home');
 
     return this._super.apply(this, arguments);
   },
--- addon/mixins/locale-route.js
+++ addon/mixins/locale-route.js
@@ -1,7 +1,11 @@
 import Ember from 'ember';
+import getLocales from 'ember-cli-i18n-locales/utils/get-locales';
 
 export default Ember.Mixin.create({
   model: function(params, transition) {
+    if (getLocales().indexOf(params.locale) === -1) {
+      this.transitionTo('/' + this.container.lookup('application:main').defaultLocale + window.location.pathname);
+    }
     if (params.locale) {
       transition.send('updateLocale', params.locale);
     }
@@ -15,8 +19,8 @@ export default Ember.Mixin.create({
     },
     changeLocale: function(locale, routeName, paramName) {
       var currentRouteName = this.controllerFor('application').get('currentRouteName');
-      var routeModel;
-      ((routeModel = { })[routeName || 'home'] = { })[paramName || 'locale'] = locale;
+      var routeModel = { };
+      (routeModel[routeName || 'home'] = { })[paramName || 'locale'] = locale;
       this.transitionToDynamic(currentRouteName, routeModel);
     }
   }
