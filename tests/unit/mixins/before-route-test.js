84ee47d23b50538490097fd03483b343b904d240
Kelly Selden <kellyselden@gmail.com>
2015-04-21 23:08:14 -0400

    new tests

--- tests/unit/mixins/before-route-test.js
+++ tests/unit/mixins/before-route-test.js
@@ -1,11 +1,12 @@
 import Ember from 'ember';
-import BeforeRouteMixin from 'ember-cli-i18n-route/mixins/before-route';
+import BeforeRouteMixin from '../../../mixins/before-route';
+import { module, test } from 'qunit';
 
 module('BeforeRouteMixin');
 
 // Replace this with your real tests.
-test('it works', function() {
+test('it works', function(assert) {
   var BeforeRouteObject = Ember.Object.extend(BeforeRouteMixin);
   var subject = BeforeRouteObject.create();
-  ok(subject);
+  assert.ok(subject);
 });
--- tests/unit/mixins/locale-route-test.js
+++ tests/unit/mixins/locale-route-test.js
@@ -1,11 +1,12 @@
 import Ember from 'ember';
-import LocaleRouteMixin from 'ember-cli-i18n-route/mixins/locale-route';
+import LocaleRouteMixin from '../../../mixins/locale-route';
+import { module, test } from 'qunit';
 
 module('LocaleRouteMixin');
 
 // Replace this with your real tests.
-test('it works', function() {
+test('it works', function(assert) {
   var LocaleRouteObject = Ember.Object.extend(LocaleRouteMixin);
   var subject = LocaleRouteObject.create();
-  ok(subject);
+  assert.ok(subject);
 });
